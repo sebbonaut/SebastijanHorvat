206d753af1d6f91a9b2acfdf4da9f3fb0f84544d
Sebastijan Horvat <sebastijan.horvatt@gmail.com>
2018-01-26 18:33:07 +0100

    Predaja zadatak (2.)

--- app/src/main/java/com/example/sehorva/kolokvij/MainActivity.java
+++ app/src/main/java/com/example/sehorva/kolokvij/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sehorva.kolokvij;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+
+        DBAdapter db = new DBAdapter(this);
+
+        //---add a contact---
+        db.open();
+        long id = db.insertContact("Wei-Meng Lee", "weimenglee@learn2develop.net");
+        id = db.insertContact("Mary Jackson", "mary@jackson.com");
+        db.close();
+
+
+
+        //--get all contacts---
+        db.open();
+        Cursor c = db.getAllContacts();
+        if (c.moveToFirst())
+        {
+            do {
+                DisplayContact(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+
+
+
+        //---get a contact---
+        db.open();
+        Cursor cu = db.getContact(2);
+        if (cu.moveToFirst())
+            DisplayContact(cu);
+        else
+            Toast.makeText(this, "No contact found", Toast.LENGTH_LONG).show();
+        db.close();
+
+
+
+        //---update contact---
+        db.open();
+        if (db.updateContact(1, "Wei-Meng Lee", "weimenglee@gmail.com"))
+            Toast.makeText(this, "Update successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Update failed.", Toast.LENGTH_LONG).show();
+        db.close();
+
+
+
+        //---delete a contact---
+        db.open();
+        if (db.deleteContact(1))
+            Toast.makeText(this, "Delete successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Delete failed.", Toast.LENGTH_LONG).show();
+        db.close();*/
+    }
+
+    private void DisplayContact(Cursor c) {
+        Toast.makeText(this,
+                "id: " + c.getString(0) + "\n" +
+                        "Name: " + c.getString(1) + "\n" +
+                        "Email:  " + c.getString(2),
+                Toast.LENGTH_LONG).show();
     }
 
     public void startService(View view) {
